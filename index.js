54f048f834cfcceb4f5d8bc36b25dbc2080c2fe5
Ishani Saravanan <ishanisaravanan@gmail.com>
2021-11-27 19:29:11 +0530

    modify index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let firstCard = 10;
-let secondCard = 11;
+let secondCard = 4;
 let sum = firstCard + secondCard;
 let cards = [firstCard, secondCard];
 let message;
@@ -14,24 +14,23 @@ function startGame() {
 }
 
 function renderGame() {
+  sumEL.textContent = "Sum: " + sum;
+  cardEL.textContent = "Cards: ";
+  for (let count = 0; count < cards.length; count++)
+    cardEL.textContent += cards[count] + " "
   if (sum < 21) {
     message = "Do you want to draw a new card?";
-    sumEL.textContent = "Sum: " + sum;
-    cardEL.textContent = "Cards: " + cards[0] + " " + cards[1];
   } else if (sum === 21) {
     message = "Wohoo! You've got Blackjack!";
-    sumEL.textContent = "Sum: " + sum;
-    cardEL.textContent = "Cards: " + cards[0] + " " + cards[1];
   } else {
     message = "You're out of the game!";
-    sumEL.textContent = "Sum: " + sum;
-    cardEL.textContent = "Cards: " + cards[0] + " " + cards[1];
   }
   messageEL.innerText = message;
 }
 
 function newCard() {
   let card = 1;
-  sum += card; //21+1 = 22
+  sum += card;
+  cards.push(card)
   renderGame();
 }
