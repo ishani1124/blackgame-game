2a5c6158b95688c71d004b5b45ac4e4151a8f191
Ishani Saravanan <ishanisaravanan@gmail.com>
2021-11-28 11:05:11 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-let firstCard = 10;
-let secondCard = 4;
-let sum = firstCard + secondCard;
-let cards = [firstCard, secondCard];
+let cards = [];
+let sum = 0;
+let hasBlackJack = false
+let isAlive = false
 let message;
 let messageEL = document.getElementById("message-el");
+
+
+console.log(cards)
 
 // let sumEL = document.getElementById("sum-el")
 let sumEL = document.querySelector("#sum-el");
 let cardEL = document.getElementById("card-el");
 
 function startGame() {
+  let isAlive = true
+  let firstCard = getRandomNumber();
+  let secondCard = getRandomNumber();
+  cards = [firstCard , secondCard]
+  sum = firstCard + secondCard
   renderGame();
+}
+
+function getRandomNumber(){
+  let randomNum =  Math.floor(Math.random()*13)+1
+  if(randomNum === 1)
+      return 11
+  else if(randomNum > 10 )
+      return 10
+  else
+     return randomNum
 }
 
 function renderGame() {
@@ -22,15 +40,21 @@ function renderGame() {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
     message = "Wohoo! You've got Blackjack!";
+    hasBlackJack = true
   } else {
     message = "You're out of the game!";
+    isAlive = false
   }
   messageEL.innerText = message;
 }
 
 function newCard() {
-  let card = 1;
+
+  if(isAlive === true && hasBlackJack === false){
+  let card = getRandomNumber();
   sum += card;
   cards.push(card)
   renderGame();
+
+  }
 }
